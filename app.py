374402a2e161db2272f82c1398455470d476a27b
Alan Ross <alanross17@me.com>
2025-01-23 12:43:34 -0800

    setup MySQL connection for storing main logs

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from config import FLASK_SECRET_KEY, PATH_TO_USERS
 from werkzeug.security import check_password_hash
 from utils import load_users_from_csv
+from db_init import init_db
 
 # Load environment variables from .env file
 load_dotenv()
@@ -19,8 +20,18 @@ app.register_blueprint(main_bp)
 print("FLASK SECRET KEY THAT WILL BE SET " + FLASK_SECRET_KEY)
 app.secret_key = FLASK_SECRET_KEY
 
-# USERNAME = os.getenv("AUTH_USERNAME")
-# HASHED_PASSWORD = os.getenv("AUTH_PASSWORD")
+# Get SQL config
+DB_CONFIG = {
+    'host': os.getenv('DB_HOST'),
+    'port': int(os.getenv('DB_PORT', 3306)),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME'),
+}
+
+# Initialize DB
+engine = init_db(DB_CONFIG)
+
 
 USERS = load_users_from_csv(PATH_TO_USERS)
 
